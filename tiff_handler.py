69c100a3da6db678a05da210d26a629320f77962
hacknus <l_stoeckli@bluewin.ch>
2022-02-17 14:33:41 +0100

    include pixels in x and y direction

--- tiff_handler.py
+++ tiff_handler.py
@@ -5,44 +5,48 @@ import matplotlib.animation as animation
 import os
 
 
-def load_series(path, return_frames=False):
+def load_series(path, ax, return_frames=False):
     i = 0
     timestamps = []
     frames = []
-    sizes = []
+    x = []
+    y = []
     path_core = path.split('_')[0] + "_" + path.split('_')[1]
     print(path_core)
     while True:
         path = f"{path_core}_{i}.tif"
         i += 1
         if os.path.exists(path):
-            timestamps_i, frames_i, sizes_i = open_tiff(path, return_frames)
+            timestamps_i, frames_i, x_i, y_i = open_tiff(path, ax, return_frames)
             timestamps += timestamps_i
             frames += frames_i
-            sizes += sizes_i
+            x += x_i
+            y += y_i
         else:
             break
-    return timestamps, frames, sizes
+    return timestamps, frames, x, y
 
 
-def open_tiff(path, return_frames=False):
+def open_tiff(path, ax, return_frames=False):
     tiff_img = Image.open(path)
     frames = []
     timestamps = []
-    sizes = []
+    x = []
+    y = []
     for i, page in enumerate(ImageSequence.Iterator(tiff_img)):
         if return_frames:
-            im = plt.imshow(page, cmap="gray")
+            im = ax.imshow(page, cmap="gray")
             frames.append([im])
         else:
             frames.append(None)
         # two's complement of the timestamp entries to get a 64bit value in nano seconds
         timestamp = np.int64((np.int64(page.tag_v2[32781]) << 32) | page.tag_v2[32782])
         timestamps.append(timestamp)
-        sizes.append(page.tag_v2[256] * page.tag_v2[257])
-        print(f"reading {i} with size {page.tag_v2[256]} x {page.tag_v2[257]}")
+        x.append(page.tag_v2[256])
+        y.append(page.tag_v2[257])
+        # print(f"reading {i} with size {page.tag_v2[256]} x {page.tag_v2[257]}")
 
-    return timestamps, frames, sizes
+    return timestamps, frames, x, y
 
 
 def get_frame_rate(timestamps):
@@ -72,39 +76,17 @@ def plot_frame_hist(timestamps, ax):
 
 
 if __name__ == "__main__":
-    # fig = plt.figure()
+    fig = plt.figure(frameon=False)
+    dpi = 1
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
 
-    timestamps, frames = open_tiff("data/image_2022-02-14T11-35-14.018_0.tif", True)
+    timestamps, frames, _, _ = open_tiff("data/image_2022-02-15T15-37-36.853_0.tif", ax, True)
 
     interval = np.diff(timestamps).mean() / 1000 / 1000 / 1000
     intervals = np.diff(timestamps) / 1000 / 1000 / 1000
     print(f"detected a framerate of {1 / interval:.2f} fps")
-    print(f"detected a inverval of {1000 * interval:.2f} ms")
-    frames = range(len(intervals))
-    plt.step(frames, intervals, color="black", where='pre')
-    plt.xlabel("frame")
-    plt.ylabel(r"interval $\Delta t$")
-    plt.savefig("interval.pdf")
-    plt.show()
+    print(f"detected an interval of {1000 * interval:.2f} ms")
+    num_frames = range(len(intervals))
 
-    n, bins, patches = plt.hist(intervals, color="black")
-    total_frames = np.sum(n) + n[-1]
-    print(f"frame drop: {n[-1] / total_frames * 100:.1f}%")
-    plt.title(f"total frames: {len(intervals)}, dropped {n[-1] / total_frames * 100:.1f}%")
-    plt.ylabel("frame number")
-    plt.xticks([min(bins), max(bins)], ["frame on time", "frame delayed"])
-    plt.savefig("hist.pdf")
-    plt.show()
-
-    exit()
-
-    interval = 50
-    fig = plt.figure()
-
-    ani = animation.ArtistAnimation(fig, frames, interval=interval, blit=True, repeat_delay=1000)
-    # save animation
-
-    # needs ffmpeg to be installed
-    mywriter = animation.FFMpegWriter(fps=24)
-    print("saving mp4...")
-    ani.save(f'test.mp4', savefig_kwargs={'facecolor': 'black'}, writer=mywriter, dpi=600)
--- tiff_to_mp4.py
+++ tiff_to_mp4.py
@@ -21,21 +21,28 @@ def fill_dropped_frames(timestamps, frames):
 
 
 if __name__ == "__main__":
-    fig = plt.figure()
+    fig = plt.figure(frameon=False)
+    dpi = 1
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
 
-    path = "data/image_2022-02-15T15-37-36.853_0.tif"
-    timestamps, frames, _ = load_series(path, True)
+    path = "data/image_2022-02-15T15-36-26.04_0.tif"
+    # path = "data/image_2022-02-15T15-37-09.663_0.tif"
+    # path = "data/image_2022-02-15T15-37-36.853_0.tif"
+    path = "data/image_2022-02-17T12-13-05.09_0.tif"
+    timestamps, frames, x, y = load_series(path, ax, True)
     frame_rate = get_frame_rate(timestamps)
-    # timestamps, frames = fill_dropped_frames(timestamps, frames)
+    timestamps, frames = fill_dropped_frames(timestamps, frames)
     print(f"{frame_rate=:.2f} s, time: {(len(timestamps) - 1) / frame_rate:.2f} s")
-
     interval = 1 / frame_rate * 1000
     print(frames)
     ani = animation.ArtistAnimation(fig, frames, interval=interval, blit=True, repeat_delay=1000)
     # save animation
-
+    fig.set_size_inches(x[0] / dpi, y[0] / dpi, forward=False)
     # needs ffmpeg to be installed
     mywriter = animation.FFMpegWriter(fps=frame_rate)
+    plt.axis('off')
     print("saving mp4...")
-    out_path = path.replace("data", "output").replace("tif","mp4")
-    ani.save(out_path, writer=mywriter)
+    out_path = path.replace("data", "output").replace("tif", "mp4")
+    ani.save(out_path, writer=mywriter, dpi=dpi)
